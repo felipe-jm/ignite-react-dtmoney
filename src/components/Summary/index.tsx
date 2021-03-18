099d039c88878b93a10c013921fd36db4a39d988
Felipe Jung <felipemattoseu@gmail.com>
2021-03-18 11:18:54 -0400

    Add totals to Summary component

--- src/components/Summary/index.tsx
+++ src/components/Summary/index.tsx
@@ -9,6 +9,25 @@ import * as S from "./styles";
 export const Summary = () => {
   const { transactions } = useContext(TransactionContext);
 
+  const summary = transactions.reduce(
+    (acc, transaction) => {
+      if (transaction.type === "deposit") {
+        acc.deposits += transaction.amount;
+        acc.total += transaction.amount;
+      } else {
+        acc.withdraws += transaction.amount;
+        acc.total -= transaction.amount;
+      }
+
+      return acc;
+    },
+    {
+      deposits: 0,
+      withdraws: 0,
+      total: 0,
+    }
+  );
+
   return (
     <S.Wrapper>
       <div>
@@ -16,21 +35,38 @@ export const Summary = () => {
           <p>Entradas</p>
           <img src={incomeImg} alt="Ícone de Entradas" />
         </header>
-        <strong>R$1000,00</strong>
+        <strong>
+          {new Intl.NumberFormat("pt-BR", {
+            style: "currency",
+            currency: "BRL",
+          }).format(summary.deposits)}
+        </strong>
       </div>
       <div>
         <header>
           <p>Saídas</p>
           <img src={outcomeImg} alt="Ícone de Saídas" />
         </header>
-        <strong>- R$500,00</strong>
+        <strong>
+          -{" "}
+          {new Intl.NumberFormat("pt-BR", {
+            style: "currency",
+            currency: "BRL",
+          }).format(summary.withdraws)}
+        </strong>
       </div>
       <div className="highlight-background">
         <header>
           <p>Entradas</p>
           <img src={totalImg} alt="Ícone de Totais" />
         </header>
-        <strong>R$500,00</strong>
+        <strong>
+          {" "}
+          {new Intl.NumberFormat("pt-BR", {
+            style: "currency",
+            currency: "BRL",
+          }).format(summary.total)}
+        </strong>
       </div>
     </S.Wrapper>
   );
